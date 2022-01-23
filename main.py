14782b9ab1038c6da2bd7c24f60c32d0519426c9
Jacy Mae Espinosa <jaymespinosa10@gmail.com>
2022-01-23 11:50:36 -0800

    Added STMPLIB to render the details entered in the form, and send an email to the admin/user

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, redirect, url_for, request, jsonify, flash
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_ckeditor import CKEditor
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from flask_bootstrap import Bootstrap
-from wtforms.validators import DataRequired, Email, email_validator
+from wtforms.validators import DataRequired, Email
 from dotenv import load_dotenv
+import smtplib
 import os
 
 load_dotenv()
@@ -21,11 +21,36 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 ckeditor = CKEditor(app)
 
+#SMTP
+MY_EMAIL = os.getenv('EMAIL')
+MY_PASSWORD = os.getenv('PASSWORD')
+TO_ADDRESS = os.getenv('TO_ADDRESS')
 
-@app.route('/')
+
+@app.route('/', methods=["GET", "POST"])
 def home():
-    form = ContactForm(request.form)
-    return render_template('index.html', form=form)
+
+    if request.method == 'POST':
+        if request.form['name'] == '' or request.form['email'] == '' or request.form['message'] == '':
+            flash('Please fill out the fields correctly.')
+            return redirect(url_for('home'))
+
+        else:
+            name = request.form['name']
+            email = request.form['email']
+            message = request.form['message']
+
+            with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+                connection.starttls()
+                connection.login(MY_EMAIL, MY_PASSWORD)
+                connection.sendmail(from_addr=MY_EMAIL,
+                                    to_addrs=TO_ADDRESS,
+                                    msg=f"Subject: You have a message!\n\n"
+                                        f"Name: {name}\n"
+                                        f"Email: {email}\n"
+                                        f"Message: {message}")
+
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
